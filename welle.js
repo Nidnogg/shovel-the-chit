40d99c5eec818119b482597e9ae6cd3f24d1bebe
fkuhnert <fvk98@yahoo.com.br>
2020-06-20 17:35:36 -0300

    First analyser canvas test

--- welle.js
+++ welle.js
@@ -7,6 +7,7 @@ const audioElement = document.querySelector('audio');
 
 // pass it into the audio context
 const track = audioContext.createMediaElementSource(audioElement);
+var analyser = audioContext.createAnalyser();
 
 track.connect(audioContext.destination);
 
@@ -35,12 +36,56 @@ audioElement.addEventListener('ended', () => {
   playButton.dataset.playing = 'false';
 }, false);
 
-// Canvas shit!
-const createCanvas = () => {
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
-  ctx.fillStyle = 'green';
-  ctx.fillRect(10, 10, 150, 100);
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const initAnalyzer = () => {
+  track.connect(analyser);
+  
+  analyser.fftSize = 2048;
 }
 
-createCanvas();
+initAnalyzer();
+
+var bufferLength = analyser.frequencyBinCount;
+var dataArray = new Uint8Array(bufferLength);
+analyser.getByteFrequencyData(dataArray);
+
+//test stuff
+var WIDTH = 300;
+var HEIGHT = 300;
+ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+function draw() {
+  var drawVisual = requestAnimationFrame(draw);
+  analyser.getByteTimeDomainData(dataArray);
+  ctx.fillStyle = 'rgb(200, 200, 200)';
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = 'rgb(0, 0, 0)';
+  ctx.beginPath();
+
+  var sliceWidth = WIDTH * 1.0 / bufferLength;
+  var x = 0;
+
+  for(var i = 0; i < bufferLength; i++) {
+   
+    var v = dataArray[i] / 128.0;
+    var y = v * HEIGHT/2;
+
+    if(i === 0) {
+      ctx.moveTo(x, y);
+    } else {
+      ctx.lineTo(x, y);
+    }
+
+    x += sliceWidth;
+  }
+  ctx.lineTo(canvas.width, canvas.height/2);
+  ctx.stroke();
+};
+
+draw();
+//finish test
+console.log("batata");
